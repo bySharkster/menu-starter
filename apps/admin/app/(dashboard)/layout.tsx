b76f70b5b7f5381a0b548b572566e14ee8d3598a
Fernando Aponte <fernandoaponte0609@gmail.com>
2025-05-06 19:42:02 -0400

    Removed unnecesary OG Image

--- apps/admin/app/(dashboard)/layout.tsx
+++ apps/admin/app/(dashboard)/layout.tsx
@@ -1,35 +1,26 @@
-import "@workspace/ui/globals.css"
-import type { Metadata } from "next"
-import Sidebar from "@/components/organisms/Sidebar"
-import RequireAuth from "@/components/templates/Wrappers/RequireAuth"
-
+import "@workspace/ui/globals.css";
+import type { Metadata } from "next";
+import Sidebar from "@/components/organisms/Sidebar";
+import RequireAuth from "@/components/templates/Wrappers/RequireAuth";
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://admin.digital-sunsets.vercel.app'),
-  title: 'Dashboard | Admin Starter',
-  openGraph: {
-    images: '/opengraph-image.png',
-    title: 'Dashboard | Admin Starter',
-    description:
-      'Dashboard | Admin Starter: A simple admin dashboard template.',
-    url: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}`,
-  },
-}
+  metadataBase: new URL("https://admin.digital-sunsets.vercel.app"),
+  title: "Dashboard | Admin Starter",
+};
 
 export default async function DashboardLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-
   return (
     <RequireAuth>
-    <div className="flex min-h-screen bg-background">
-      <Sidebar />
-      <main className="flex-1 p-6 flex flex-col gap-6">
-        <div className="z-10">{children}</div>
-      </main>
-    </div>
+      <div className="flex min-h-screen bg-background">
+        <Sidebar />
+        <main className="flex-1 p-6 flex flex-col gap-6">
+          <div className="z-10">{children}</div>
+        </main>
+      </div>
     </RequireAuth>
-  )
+  );
 }
