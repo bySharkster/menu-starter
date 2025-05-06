de5062076156f2070198eee8d69b5aa9119a4796
Fernando Aponte <fernandoaponte0609@gmail.com>
2025-05-06 19:38:49 -0400

    bug: removed duplicate OG image

--- apps/admin/app/layout.tsx
+++ apps/admin/app/layout.tsx
@@ -1,49 +1,41 @@
-import { Geist, Geist_Mono } from "next/font/google"
-import "@workspace/ui/globals.css"
-import { Providers } from "@/components/templates/Wrappers/providers"
-import type { Metadata } from "next"
-import { getServerSession } from "next-auth"
-import { authOptions } from "@/utils/authOptions"
-import { Toaster } from "@workspace/ui/components/sonner"
+import { Geist, Geist_Mono } from "next/font/google";
+import "@workspace/ui/globals.css";
+import { Providers } from "@/components/templates/Wrappers/providers";
+import type { Metadata } from "next";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/utils/authOptions";
+import { Toaster } from "@workspace/ui/components/sonner";
 
 const fontSans = Geist({
   subsets: ["latin"],
   variable: "--font-sans",
-})
+});
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
   variable: "--font-mono",
-})
+});
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://admin.digital-sunsets.vercel.app'),
-  title: 'Admin Starter',
-  openGraph: {
-    images: '/opengraph-image.png',
-    title: 'Admin Starter',
-    description:
-      'Admin Starter: A simple admin dashboard template.',
-    url: `${process.env.NEXT_PUBLIC_NEXTAUTH_URL}`,
-  },
-}
+  metadataBase: new URL("https://admin.digital-sunsets.vercel.app"),
+  title: "Admin Starter",
+};
 
 export default async function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
-  const session = await getServerSession(authOptions)
-
+  const session = await getServerSession(authOptions);
 
   return (
-    <html lang="en" suppressHydrationWarning >
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
       >
         <Providers session={session}>{children}</Providers>
-        <Toaster/>
+        <Toaster />
       </body>
     </html>
-  )
+  );
 }
