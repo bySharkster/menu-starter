8c2fb34d7abcb82364cc52d6e284e67da36e8eed
Fernando Aponte <fernandoaponte0609@gmail.com>
2025-05-06 20:31:51 -0400

    chore: added static root layout og

--- apps/admin/app/(dashboard)/menu/items/layout.tsx
+++ apps/admin/app/(dashboard)/menu/items/layout.tsx
@@ -1,9 +1,36 @@
+import type { Metadata } from "next";
 
-export default function MenuItemLayout({children, modal}: {children: React.ReactNode, modal: React.ReactNode}) {
-    return (
-        <div>
-            {modal && <div>{modal}</div>}
-            <div>{children}</div>   
-        </div>
-    )
+export const metadata: Metadata = {
+  metadataBase: new URL("https://menu-starter-admin.vercel.app/"),
+  title: "Menu Items | Menu Starter",
+  openGraph: {
+    title: "Menu Items | Menu Starter",
+    description: "Menu Starter",
+    type: "website",
+    url: "https://menu-starter-admin.vercel.app/menu/items",
+    siteName: "Menu Starter",
+    images: [
+      {
+        url: "https://menu-starter-admin.vercel.app/api/og?title=Menu Starter&description=Menu Starter",
+        width: 1200,
+        height: 630,
+        alt: "Menu Starter",
+      },
+    ],
+  },
+};
+
+export default function MenuItemLayout({
+  children,
+  modal,
+}: {
+  children: React.ReactNode;
+  modal: React.ReactNode;
+}) {
+  return (
+    <div>
+      {modal && <div>{modal}</div>}
+      <div>{children}</div>
+    </div>
+  );
 }
--- apps/admin/app/(dashboard)/menu/layout.tsx
+++ apps/admin/app/(dashboard)/menu/layout.tsx
@@ -1,3 +1,25 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://menu-starter-admin.vercel.app/"),
+  title: "Menu | Menu Starter",
+  openGraph: {
+    title: "Menu | Menu Starter",
+    description: "Menu Starter",
+    type: "website",
+    url: "https://menu-starter-admin.vercel.app/menu",
+    siteName: "Menu Starter",
+    images: [
+      {
+        url: "https://menu-starter-admin.vercel.app/api/og?title=Menu Starter&description=Menu Starter",
+        width: 1200,
+        height: 630,
+        alt: "Menu Starter",
+      },
+    ],
+  },
+};
+
 export default function MenuLayout({
   children,
   modal,
